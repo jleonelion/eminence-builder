9153d959b33efedd0fd7ffa47e3c61c623d469dc
jleonelion <1558486+jleonelion@users.noreply.github.com>
2025-01-21 00:07:36 -0800

    fix conversion of config, use Message types, placeholder for subgraph

--- agents/generate_post/graph.py
+++ agents/generate_post/graph.py
@@ -16,28 +16,31 @@ import random
 from agents.utils import load_chat_model
 from agents.generate_post.state import GeneratePostState
 from agents.generate_post.configuration import GeneratePostConfiguration
+from langchain_core.messages import SystemMessage, HumanMessage
+from langchain_core.runnables import RunnableConfig
 from agents.generate_post.utils import *
-from agents.contstants import ALLOWED_TIMES
-
+from agents.verify_source.graph import graph as verify_links_graph
 
 async def generate_report(
-    state: GeneratePostState, *, config: GeneratePostConfiguration
+    state: GeneratePostState, *, config: RunnableConfig
 ) -> GeneratePostState:
     """Create report content."""
     
+    config = GeneratePostConfiguration.from_runnable_config(config)
     # call model to generate the report
     model = load_chat_model(config.report_model)
-    messages = [
-        {"role": "system", "content": build_report_system_prompt(state, config)},
-        {"role": "human", "content": build_report_content_prompt(state, config)},
-    ]
-    response = await model.ainvoke(messages)
+    response = await model.ainvoke(
+        [
+            SystemMessage(build_report_system_prompt(state, config)),
+            HumanMessage(build_report_content_prompt(state, config)),
+        ]
+    )
     return {
         "report": parse_report(response["content"]),
     }
 
 async def generate_post(
-    state: GeneratePostState, *, config: GeneratePostConfiguration
+    state: GeneratePostState, *, config: RunnableConfig
 ) -> GeneratePostState:
     """Create post content."""
     
@@ -45,13 +48,16 @@ async def generate_post(
         raise ValueError("No report found.")
     if len(state.relevant_links) == 0:
         raise ValueError("No relevant links found.")
+    
+    config = GeneratePostConfiguration.from_runnable_config(config)
     # call model to generate the post
     model = load_chat_model(config.post_model)
-    messages = [
-        {"role": "system", "content": build_post_system_prompt(state, config)},
-        {"role": "human", "content": build_report_prompt(state, config)},
-    ]
-    response = await model.ainvoke(messages)
+    response = await model.ainvoke(
+        [
+            SystemMessage(build_post_system_prompt(state, config)),
+            HumanMessage(build_report_prompt(state, config)),
+        ]
+    )
     # calculate post during time for next Saturday
     next_saturday = get_next_saturday()
     random_hour = random.uniform(8, 17) # 8am to 5pm
@@ -62,12 +68,15 @@ async def generate_post(
         "schedule_date": schedule_date,
     }
 
-
 # Define the graph
-builder = StateGraph(GeneratePostState, GeneratePostConfiguration)
+builder = StateGraph(GeneratePostState, config_schema=GeneratePostConfiguration)
+# TODO: add verify links subgraph
+# builder.add_node("verify_links_graph", verify_links_graph)
 builder.add_node(generate_post)
 builder.add_node(generate_report)
 builder.add_edge(START, "generate_report")
+# builder.add_edge(START, "verify_links_graph")
+# builder.add_edge("verify_links_graph", "generate_report")
 builder.add_edge("generate_report", "generate_post")
 builder.add_edge("generate_post", END)
 # Compile into a graph object that you can invoke and deploy.
