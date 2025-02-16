25c0a1bfab4639063b037fd80f94cabf0386ef24
jleonelion <1558486+jleonelion@users.noreply.github.com>
2025-02-16 01:00:52 -0800

    test hook

--- scratchpad/langgraph_server.py
+++ scratchpad/langgraph_server.py
@@ -1,17 +1,22 @@
-import asyncio
-from langgraph_sdk import get_client, get_sync_client
+
 from langgraph.pregel.remote import RemoteGraph
-from agents.reflection.state import ReflectionState
+from langgraph_sdk import get_client, get_sync_client
+
 from agents.generate_post.configuration import GeneratePostConfiguration
+from agents.reflection.state import ReflectionState
 
 url = "http://localhost:2024"
 graph_name = "reflection"
 
+
 async def main():
-    client = get_client(url=url)
-    sync_client = get_sync_client(url=url)
     remote_graph = RemoteGraph(graph_name, url=url)
-    await remote_graph.ainvoke(input=ReflectionState(original_text="Hello, world!", revised_text="Hello, world!"))
+    await remote_graph.ainvoke(
+        input=ReflectionState(
+            original_text="Hello, world!", revised_text="Hello, world!"
+        )
+    )
+
 
 config = GeneratePostConfiguration()
 # asyncio.run(main())
